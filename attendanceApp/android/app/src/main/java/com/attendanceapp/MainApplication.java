7f1323383efc6d9b1a8771a915eda76d77fbfb5e
abhishek71994 <abhishek71994@gmail.com>
2018-04-19 10:17:32 +0530

    reverting changes

--- attendanceApp/android/app/src/main/java/com/attendanceapp/MainApplication.java
+++ attendanceApp/android/app/src/main/java/com/attendanceapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.attendanceapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
+import com.filepicker.FilePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDocumentPicker(),
+            new FilePickerPackage(),
             new VectorIconsPackage()
       );
     }
--- attendanceApp/components/upload.js
+++ attendanceApp/components/upload.js
@@ -4,10 +4,31 @@ import {
 	Text,
 	TextInput,
 	View,
+	TouchableOpacity,
 	Button
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
 export default class Upload extends Component{
+	constructor(props){
+		super(props);
+		this.uploadFile = this.uploadFile.bind(this);
+	}
+	uploadFile() {
+		console.log('Upload pressed');
+
+		DocumentPicker.show({
+	      filetype: [DocumentPickerUtil.pdf()],
+	    },(error,res) => {
+	      // Android
+	      console.log(
+	         res.uri,
+	         res.type, // mime type
+	         res.fileName,
+	         res.fileSize
+	      );
+	    });
+	} 
 	render(){
 		return(
 			<ScrollView style={{padding: 20}}>
@@ -15,9 +36,10 @@ export default class Upload extends Component{
 				<Icon
 				name='plus-circle'
 				size={300}
+				onPress={this.uploadFile}
 				style={{ marginLeft:40 }}
 				/>
-				<Button title="Submit"/>
+				<TouchableOpacity><Text>Upload File</Text></TouchableOpacity>
 			</ScrollView>
 			)
 	}
