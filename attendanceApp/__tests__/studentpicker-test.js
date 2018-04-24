69c9467c518c4fc2459c4d5d73027ce9b1e0dc46
abhishek71994 <abhishek71994@gmail.com>
2018-04-25 00:55:06 +0530

    test cases

---
+++ attendanceApp/__tests__/studentpicker-test.js
@@ -0,0 +1,20 @@
+import 'react-native';
+import React from 'react';
+// import Login from '../components/login';
+// import App from '../App';
+import StudentPicker from '../components/studentpicker';
+// import StudentTicket from '../components/studentticket';
+// import Upload from '../components/upload';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    //<Login />
+    <StudentPicker />
+    //<App />
+    //<StudentTicket />
+    //<Upload />
+  );
+});
