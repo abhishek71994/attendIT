c9e27f647c7082d2d66d12b32f1f3f88d2bf91da
abhishek71994 <abhishek71994@gmail.com>
2018-05-04 12:26:41 +0530

    added count feature

--- attendanceApp/backend/server/modules/tickets/controller.js
+++ attendanceApp/backend/server/modules/tickets/controller.js
@@ -3,7 +3,7 @@ import nodeMailer from 'nodemailer';
 import MongoClient from 'mongodb';
 export const createTicket = async (req,res) => {
 	const { name, enrollment_no, date, event, department,section,year } = req.body;
-	const day = date.getDay();
+	const day = "Saturday";
 	const newTicket = new Ticket({ name, enrollment_no, date, event, department, section, year });
 	try{
 		// const transporter = nodeMailer.createTransport({
@@ -27,12 +27,9 @@ export const createTicket = async (req,res) => {
 		// })
 		MongoClient.connect('mongodb://localhost/', (err,client) =>{
 			if (err) console.log(err);
-			
 			const db = client.db('studentTicket');
-			//get the array of approves students
-			//loop through those students to find the right ones.
-			// send email to them and 
-			db.collection('tickets').update({year : this.year , section : this.section , department: this.department, day: this.day })
+			// update the count
+			db.collection('count').update({year : year , section : section , department: department, day: day },{ $inc:{"count":1} })
 		});
 
 		return res.status(201).json({ticket: await newTicket.save()});
