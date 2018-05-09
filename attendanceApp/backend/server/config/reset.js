d6ad81871fe9f23ca52dada3967fae5e4c4bfc48
abhishek71994 <abhishek71994@gmail.com>
2018-05-09 10:04:51 +0530

    added cron job feature

--- attendanceApp/backend/server/config/reset.js
+++ attendanceApp/backend/server/config/reset.js
@@ -1,4 +1,7 @@
 import Schedule from 'node-schedule';
+import MongoClient from 'mongodb';
+import nodeMailer from 'nodemailer';
+
 export default ()=>{
 	Schedule.scheduleJob('*/5 * * * * *',function(){
 		console.log("Hurricane!");
@@ -9,16 +12,59 @@ export default ()=>{
 	rule.dayOfWeek = new schedule.Range(2, 6);//from tuesday to saturday
 	rule.hour = 9;
 	rule.minute = 5;
-
+		
 
 	const deprecationRule = new Schedule.RecurrenceRule();
 	deprecationRule.dayOfWeek = new schedule.Range(2, 6);//from tuesday to saturday
-	deprecationRule.hour = 10;
+	deprecationRule.hour = 22;
 	deprecationRule.minute = 30;
 
 
 	const j = schedule.scheduleJob(rule, function(){
 		//notify the teacher about the tickets.
+		db.collection('count').find().toArray((err, data) =>{
+				if (err) console.log(err)
+				else{
+					if(data.count > 0){
+						db.collection('routine').find({day:data.day,year: data.year,section: data.section,department:data.department}).toArray((err, roudata) =>{
+							if (err) console.log(err)
+							else{
+								if(roudata.length !== 0){
+									roudata.routine.foEach((teach)=>{
+										db.collection('teacher').find({ name:teach }).toArray((err, data) =>{
+											if (err) console.log(err)
+											else{
+												const transporter = nodeMailer.createTransport({
+													service : 'Gmail',
+													auth : {
+														user : 'xrace2018@gmail.com',
+														pass : ''
+													}
+												});
+												const message = {
+													from : 'xrace2018@gmail.com',
+													to : 'abhishek71994@gmail.com',
+													subject : 'Ticket notification',
+													text : `${name} raised a ticket to not attend class on ${date}`,
+													html : `<p>${name} raised a ticket to not attend class on ${date}</p>`,
+												}
+												transporter.sendMail(message,(err,res)=>{
+													if(err) console.log(err);
+
+													console.log(res);
+												})
+												
+											}
+										});
+									})
+								}
+								
+							}
+						});
+					}
+					
+				}
+			});
 		//check the count and tell about a backup class
 	  console.log('Today is recognized by Rebecca Black!');
 	});
